eefad23dc8fa9d11c343aaff8013f1ec4d15c977
Shivam Wayal <wayalshivam7@gmail.com>
2026-05-09 16:51:20 +0000

    refactor: update Task and User DTOs for improved data handling

--- src/main/java/com/portfolio/task_management_system/dto/TaskDTO.java
+++ src/main/java/com/portfolio/task_management_system/dto/TaskDTO.java
@@ -1,5 +1,7 @@
 package com.portfolio.task_management_system.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TaskDTO {
+public class TaskDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private Long id;
     private String title;
     private String description;
--- src/main/java/com/portfolio/task_management_system/dto/UserDTO.java
+++ src/main/java/com/portfolio/task_management_system/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.portfolio.task_management_system.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private Long id;
     private String name;
     private String email;
